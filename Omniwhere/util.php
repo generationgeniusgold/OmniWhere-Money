2ffbadf6eee807d203c7bd391cd33072bc063f1f
generationgeniusgold <109559328+generationgeniusgold@users.noreply.github.com>
2022-07-19 01:09:56 +0200

    Add files via upload

---
+++ Omniwhere/util.php
@@ -0,0 +1,28 @@
+<?php
+
+    class Util
+    {
+        // DB Variables
+        static $DB_NAME = "<Name of your database>";
+        static $DB_USER = "<DBMS username>";
+        static $DB_PASSWORD = "<DBMS password>";
+        static $SERVER_NAME = "localhost";
+
+        // USSD Variables
+        static $GO_BACK  = "98";
+        static $GO_TO_MAIN_MENU = "99";
+
+        // User initial balance
+        static $USER_BALANCE = 4000;
+
+        static $COUNTRY_CODE = "+225";
+
+        static $TRANSACTION_FEE = 50;
+
+        //Omniwhere Money Credentials
+        static $API_KEY = <"API Key from Omniwhere Money">
+        static $API_USERNAME = "sandbox";
+        static $AT_SHORTCODE = "<Your shortcode>";
+        static $COMPANY_NAME = "<Your company name>";
+    }
+?>
